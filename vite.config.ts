fee2f226dfdbf3035e704d6ffbbc3b8f9cc0f6e5
Ravurimadan <ravurimadan@gmail.com>
2025-10-27 15:54:02 +0530

    new update

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// ESM __dirname shim
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname),
   base: './',
   publicDir: 'public',
   plugins: [react()],
@@ -31,7 +28,7 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html')
+        main: path.resolve(__dirname, 'index.html'),
       },
       output: {
         manualChunks(id) {
