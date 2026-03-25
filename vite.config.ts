09c326777d42717b84066d6183fd5e0d1b5f27af
Oryprojects <k.ravi@oryfolks.com>
2026-03-25 17:19:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: '/visionai/', // 👈 IMPORTANT for ingress path
+  base: '/', // 👈 IMPORTANT for ingress path
 
   plugins: [react()],
 
